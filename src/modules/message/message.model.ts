9e58775fbc532517ac70321564b09e02f5bc9060
muntasir010 <naeemtasir03@gmail.com>
2026-03-13 03:37:47 +0600

    feat: create message model

--- src/modules/message/message.model.ts
+++ src/modules/message/message.model.ts
@@ -1,7 +1,15 @@
-export interface IMessage {
-  firstName: string;
-  lastName: string;
-  email: string;
-  subject: string;
-  message: string;
-}
+import { Schema, model } from 'mongoose';
+import { IMessage } from './message.interface';
+
+const messageSchema = new Schema<IMessage>(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    subject: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Message = model<IMessage>('Message', messageSchema);
