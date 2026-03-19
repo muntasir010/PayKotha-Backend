274bcbfeb976af8f3ec3b1b20cc58631b932f00e
muntasir010 <naeemtasir03@gmail.com>
2026-03-20 00:07:26 +0600

    feat: update node fetch to axios

--- src/modules/ai/ai.service.ts
+++ src/modules/ai/ai.service.ts
@@ -1,14 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// src/services/ai.service.ts
 import { GoogleGenAI } from "@google/genai";
-import fetch from "node-fetch";
 import { enVars } from "../../config/env";
 
 const gemini = new GoogleGenAI({ apiKey: enVars.GEMINI_API_KEY });
 
 export const generateAIResponse = async (message: string) => {
   try {
-    // Try Gemini first
     const gemResponse = await gemini.models.generateContent({
       model: "gemini-2.0-flash",
       contents: `You are PayKotha AI assistant.\nUser: ${message}`,
@@ -16,7 +13,7 @@ export const generateAIResponse = async (message: string) => {
     return gemResponse.text;
   } catch (e: any) {
     if (e.status === 429 || e.status === 403) {
-      // Fallback to Hugging Face
+      
       const hfResponse = await fetch(
         "https://api-inference.huggingface.co/models/gpt2",
         {
@@ -26,9 +23,10 @@ export const generateAIResponse = async (message: string) => {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({ inputs: message }),
-        },
+        }
       );
-      const data = await hfResponse.json();
+
+      const data: any = await hfResponse.json();
 
       if (Array.isArray(data) && data[0]?.generated_text) {
         return data[0].generated_text;
